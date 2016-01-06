e2bf1e0bf72241feaf849a35cc2a92bc7470e33f
Mike Barry <msb5014@gmail.com>
2016-01-06 06:04:00 -0500

    lightweight output

--- scripts/serve.js
+++ scripts/serve.js
@@ -14,10 +14,25 @@ if (PROD) {
 } else {
   var webpack = require('webpack');
   var config = require('../webpack.config');
+  var ProgressPlugin = require('webpack/lib/ProgressPlugin');
   var compiler = webpack(config);
+  compiler.apply(new ProgressPlugin(function(percentage, msg) {
+    process.stdout.write(`  ${msg} ${(percentage * 100).toFixed(0)}%\r`);
+  }));
 
   app.use(require('webpack-dev-middleware')(compiler, {
     publicPath: config.output.publicPath,
+    quiet: false,
+    noInfo: false,
+    stats: {
+      assets: false,
+      colors: true,
+      version: false,
+      hash: false,
+      timings: false,
+      chunks: false,
+      chunkModules: false
+    }
   }));
 
   app.use(require('webpack-hot-middleware')(compiler));
